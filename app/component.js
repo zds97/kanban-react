d71f605141ccffc35620b121943919b1fc66b3ae
Zach Sebag <zsebag@gmail.com>
2015-10-22 16:57:04 -0500

    Updated webpack config settings Added base files for application Updated package.json with script

---
+++ app/component.js
@@ -0,0 +1,5 @@
+module.exports = function(){
+  var element = document.createElement('hi');
+  element.innerHTML = "Hello World";
+  return element;
+};
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin')
+var webpack = require('webpack');
+
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = {
+  entry: APP_PATH,
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js'
+  },
+    devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
+  ]
+
+};
