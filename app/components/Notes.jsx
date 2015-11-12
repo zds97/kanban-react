c70e18f3a1bd7ce7bca55b786db97da20eb08255
Zach Sebag <zsebag@gmail.com>
2015-11-12 17:34:42 -0600

    Added component for individual notes Refactored logic to separate component

---
+++ app/components/Notes.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Note from './Note.jsx'
+
+export default class Notes extends React.component {
+  render(){
+    const notes = this.props.items;
+
+    return <ul className="notes">{notes.map(this.renderNote)}</ul>
+  }
+
+  renderNote(note){
+    return (
+      <li className="note" key={note.id}>
+        <Note task={note.task} />
+      </li>
+    );
+  }
+}
